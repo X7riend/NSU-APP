de17421b9211a07af7f7ff05a3fab98e204fbfcf
reizayah <reizayah@gmail.com>
2023-11-04 14:02:23 -0400

    Test service

--- app.py
+++ app.py
@@ -74,10 +74,12 @@ if __name__ == '__main__':
     service.passengers = [
         {"id": 1, "name": "John"},
         {"id": 2, "name": "Alice"},
+        {"id": 3, "name": "Joseph"},
     ]
     # Sample passenger preferences
     service.setMealPreferences(1, [1, 2])
     service.setMealPreferences(2, [2, 3])
+    service.setMealPreferences(3, [3, 1])
     # Process orders
     service.processOrders()
 
